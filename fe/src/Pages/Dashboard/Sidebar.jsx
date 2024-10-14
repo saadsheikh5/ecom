b0e1491be417142e0a75bc2acb399874cabf27b7
saad ali <saadalisheikh2002@gmail.com>
2024-10-14 14:44:03 -0700

    Initial commit

--- fe/src/Pages/Dashboard/Sidebar.jsx
+++ fe/src/Pages/Dashboard/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Ensure you have react-router-dom installed
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
